b2d8379846995b20ccde19ae3128506d20b43ca1
markimrec <39174573+markimrec@users.noreply.github.com>
2018-05-15 21:09:20 -0400

    Add project files.

---
+++ MyEcomerce/Controllers/ProdutosController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyEcomerce.Controllers
+{
+    public class ProdutosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Criar()
+        {
+            return View();
+        }
+    }
+}

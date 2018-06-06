1f6dbb4eec56e581321e7d1aace19756e767cb4c
markimrec <39174573+markimrec@users.noreply.github.com>
2018-06-05 23:32:37 -0400

    Cria tela de usuários

---
+++ MyEcomerce/Controllers/UsuarioController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MyEcomerce.Models;
+
+namespace MyEcomerce.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        public IActionResult Index()
+        {
+
+
+            return View();
+        }
+
+
+
+        public IActionResult Criar()
+        {
+            
+            return View();
+        }
+
+
+        public ActionResult Salvar(Usuario usuario)
+        {
+            if (usuario == null)
+            {
+                throw new ArgumentNullException(nameof(usuario));
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ MyEcomerce/Models/Usuario.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyEcomerce.Models
+{
+    public class Usuario
+    {
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+        public string Senha { get; set; }
+
+        private string _nomeDaMae;
+        public string NomeDaMae
+        {
+            get { return _nomeDaMae;  }
+            set { _nomeDaMae = value; }
+        }
+
+    }
+}

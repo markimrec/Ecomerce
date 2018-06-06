177a8a363035fa8ad7da3572e495d1dfe140db85
markimrec <39174573+markimrec@users.noreply.github.com>
2018-06-05 21:41:41 -0400

    initial commit

--- MyEcomerce/Controllers/ProdutosController.cs
+++ MyEcomerce/Controllers/ProdutosController.cs
@@ -3,14 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MyEcomerce.Models;
 
 namespace MyEcomerce.Controllers
 {
     public class ProdutosController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Index(int id = 0)
         {
-            return View();
+            var produtoParaRetornar = new Produto
+            {
+                Id = 11,
+                IdFornecedor = 3,
+                Name = "Produto do markim"
+            };
+
+
+            var meuProduto = new Produto();
+            meuProduto.Id = 11;
+            meuProduto.IdFornecedor = 3;
+            meuProduto.Name = "Produto";
+
+
+
+
+            return View(produtoParaRetornar);
         }
 
         public IActionResult Criar()
---
+++ MyEcomerce/Models/Produto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyEcomerce.Models
+{
+    public class Produto
+    {
+        [DisplayName("Número de Identificação do Fornecedor")]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int IdFornecedor { get; set; }
+
+    }
+}
